bc7184a43904256280dc2981121a06c04915fb39
卢智超 <luzhichao@zhongzijinfu.com>
2017-09-04 14:29:29 +0800

    change

--- sample/src/main/java/com/example/luzhichao/datepickerproject/MainActivity.java
+++ sample/src/main/java/com/example/luzhichao/datepickerproject/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.luzhichao.datepickerproject;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
